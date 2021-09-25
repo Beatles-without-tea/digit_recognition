164569bcdbaf7f4ae1f80367caee1ecd95cdfc04
mbaexales <max.beales@gmail.com>
2021-09-25 17:02:54 +0200

    updates

--- midterm.py
+++ midterm.py
@@ -67,10 +67,20 @@ index_of_value_to_predict=1
 y_hat_test=model.predict(x_test_norm)
 #plot the number from the testing set 
 plt.imshow(x_test[index_of_value_to_predict],cmap=plt.get_cmap('gray'))
+plt.show()
 #prediction given by the model
 prediction_1=np.where(y_hat_test[index_of_value_to_predict,:]==max(y_hat_test[index_of_value_to_predict,:]))[0][0]
 print('\nThe true label of the value in the testing set is ',y_test[index_of_value_to_predict],'\nThe predicted value is ',prediction_1)
+#View network predictions (probabilities of class membership)
+plt.bar(range(0,10),list(y_hat_test[index_of_value_to_predict,:]))
+plt.title('Probabilities of class membership')
+plt.ylabel('probability')
+plt.xlabel('number')
+plt.show()
 
+#evaluate on mnist dataset
+scores = model.evaluate(x_test_norm, y_test)
+print('The NN model gives us an accuracy of '+str(scores[1])+' and a loss of '+str(scores[0]))
 ###############Architecture B of a convolutional neural network
 #change shape of data for convolution
 
@@ -80,23 +90,23 @@ x_train_dims=np.expand_dims(x_train/255,axis=-1)
 x_test_dims=np.expand_dims(x_test/255,axis=-1)
 #model 
 
-model = keras.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
-model.compile(optimizer='adam',
+model_cnn = keras.Sequential()
+model_cnn.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model_cnn.add(layers.MaxPooling2D((2, 2)))
+model_cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model_cnn.add(layers.MaxPooling2D((2, 2)))
+model_cnn.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model_cnn.add(layers.Flatten())
+model_cnn.add(layers.Dense(64, activation='relu'))
+model_cnn.add(layers.Dense(10))
+model_cnn.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 #illustrate with diagram using function in keras 
-keras.utils.plot_model(model,show_shapes=True)
+keras.utils.plot_model(model_cnn,show_shapes=True)
 #fit the model
-history_cnn = model.fit(x_train_dims, y_train, epochs=10,validation_split=0.2 )
+history_cnn = model_cnn.fit(x_train_dims, y_train, epochs=epoch_number,validation_split=0.2 )
 #plot training and validation loss and accuracy
 fig,axs=plt.subplots(1,2,figsize=(10,5))
 fig.suptitle('CNN Model training and loss')
@@ -114,5 +124,24 @@ axs[1].set_title('Model loss')
 axs[1].legend()
 plt.show()
 
+#predictions
+#choose an index 
+index_of_value_to_predict_cnn=11
+#get predictions on testing data
+y_hat_test_cnn=model_cnn.predict(x_test_dims)
+#plot the number from the testing set 
+plt.imshow(x_test[index_of_value_to_predict_cnn],cmap=plt.get_cmap('gray'))
+plt.show()
+#prediction given by the model
+prediction_1_cnn=np.where(y_hat_test_cnn[index_of_value_to_predict_cnn,:]==max(y_hat_test_cnn[index_of_value_to_predict_cnn,:]))[0][0]
+print('\nThe true label of the value in the testing set is ',y_test[index_of_value_to_predict_cnn],'\nThe predicted value using the CNN model is ',prediction_1_cnn)
+plt.bar(range(0,10),list(y_hat_test_cnn[index_of_value_to_predict_cnn,:]))
+plt.title('Probabilities of class membership')
+plt.ylabel('probability')
+plt.xlabel('number')
+plt.show()
+
+scores_cnn = model_cnn.evaluate(x_test_dims, y_test)
+print('\nThe NN model gives us an accuracy of '+str(scores_cnn[1])+' and a loss of '+str(scores_cnn[0]))
 
 
